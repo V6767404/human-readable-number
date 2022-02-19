6cae6332b986fbd312e2560bc5a1d48281ba9dd6
V6767404 <32267531+V6767404@users.noreply.github.com>
2022-02-19 14:38:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function toReadable (number) {
+    
+    let oneDigit = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let twoDigit = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let threeDigit = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let hundreds = Math.floor(number / 100);
+    let dozens = number % 100;
+    let units = dozens % 10;
+    let result = [];
+
+    if (number === 0)
+        return 'zero';
+
+    if (hundreds > 0) {
+        result.push(oneDigit[hundreds] + ' hundred');
+    }
+    
+    if (dozens >= 20) {
+        result.push(threeDigit[Math.floor(dozens / 10)]);
+        if (units !== 0) {
+            result.push(oneDigit[units]);
+        }
+    } else if (dozens >= 10) result.push(twoDigit[dozens % 10]);
+    else if (dozens !== 0) result.push(oneDigit[units]);
+
+    // return result;
+    return result.join(' ');
   
 }
